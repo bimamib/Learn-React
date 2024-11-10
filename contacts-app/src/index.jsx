6386e1918df7ce99585309f73866d7bc0b7c3ee3
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-10 19:00:01 +0700

    Update import

--- contacts-app/src/index.jsx
+++ contacts-app/src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ContactApp from "./ContactApp";
+import ContactApp from "./components/ContactApp";
 
 const element = <h1>Hello, World!</h1>;
+
+// Styling
+import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<ContactApp />);
