7536df78734c12faa5e62f586305cbe4e95e4bbf
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-12 06:18:26 +0700

    Membuat dua method

---
+++ component-state/src/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi nilai count di dalam state
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1,
+      };
+    });
+  }
+
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>TODO: selesaikan component-nya!</p>
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<CounterApp />);
